9ca61839aaf8dfea44bf5931d431e4e6f9d88469
Vishnu-Marupaka <vishnumarupaka2006@gmail.com>
2025-06-26 20:50:35 +0530

    Create test7.py

---
+++ projects/test7.py
@@ -0,0 +1,14 @@
+#list operations
+a=["pears","mango","banana","apple"] 
+print(a) 
+print(type(a)) 
+print(a[-3]) 
+print(a[2:]) 
+print(a*3) 
+a.append("grapes") 
+print(a) 
+a.extend("papaya") 
+print(a) 
+a.sort() 
+print(a)  
+print(a.pop())
